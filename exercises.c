db661439166cc72b018cd210a01c9625f1b6e8b8
Your Name <you@example.com>
2024-03-23 20:28:30 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -130,17 +130,17 @@ paraéntesis balanceados. Retorna 1 si están balanceados,
 0 en caso contrario.
 */
 
-int parentesisBalanceados(char *cadena) {
+int parentesisBalanceados(char* cadena) {
     List* stack = create_list();
     for (int i = 0; cadena[i] != '\0'; i++) {
-        if (cadena[i] == '('  cadena[i] == '{'  cadena[i] == '[') {
-            pushFront(stack, &(cadena[i]));
-        } else if (cadena[i] == ')'  cadena[i] == '}'  cadena[i] == ']') {
+        if (cadena[i] == '(' || cadena[i] == '{' || cadena[i] == '[') {
+            pushFront(stack, cadena[i]);
+        } else if (cadena[i] == ')' || cadena[i] == '}' || cadena[i] == ']') {
             if (get_size(stack) == 0) {
                 return 0;
             }
-            char last = (char)first(stack);
-            if ((last == '(' && cadena[i] == ')')  (last == '{' && cadena[i] == '}')  (last == '[' && cadena[i] == ']')) {
+            char last = stack->head->data;
+            if ((last == '(' && cadena[i] == ')') || (last == '{' && cadena[i] == '}') || (last == '[' && cadena[i] == ']')) {
                 popFront(stack);
             } else {
                 return 0;
